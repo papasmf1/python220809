3dd9c657d8e00b0aeeb21789f9c80e2aeb415b90
papasmf1 <papasmf1@gmail.com>
2022-08-09 16:16:21 +0900

    두번째 클래스 연습

--- class1.py
+++ class1.py
@@ -3,12 +3,16 @@
 class Person:
     #약속된 이름으로 초기화 메서드 정의
     def __init__(self):
+        #초기화 루틴 
         self.name = "default name"
     def print(self):
         print("My name is {0}".format(self.name))
 
 #2)인스턴스(복사본)을 생성
-p1 = Person()
+p1 = Person() 
+p2 = Person() 
+p1.name = "전우치"
 #3)메서드 호출
 p1.print()
+p2.print() 
 
