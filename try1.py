662ca23ccf680961533100e3e5a8f965863cae82
papasmf1 <papasmf1@gmail.com>
2022-08-10 15:11:04 +0900

    에러 처리 연습

---
+++ try1.py
@@ -0,0 +1,20 @@
+# try1.py
+#함수 정의
+def divide(a,b):
+    return a/b 
+#에러 처리
+try:
+    #호출
+    result = divide(5,2)
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다.")
+except TypeError:
+    print("숫자 형식이어야 합니다.")
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("한번 더 체크(이중 체크)")
+
+print("---전체 실행 종료---")
+
+
