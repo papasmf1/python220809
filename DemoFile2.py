41dbe973506f1f3a928311f6f826d5acd09b0978
papasmf1 <papasmf1@gmail.com>
2022-08-10 17:01:11 +0900

    파일 읽기, 쓰기 연습

---
+++ DemoFile2.py
@@ -0,0 +1,27 @@
+# DemoFile2.py
+
+#파일쓰기
+#f = open("c:\\work\\test.txt", "wt", encoding="utf-8")
+#raw string notation(r)
+#f = open(r"c:\work\test2.txt", "wt", encoding="utf-8")
+#linu, unix
+f = open("c:/work/test3.txt", "wt", encoding="utf-8")
+
+f.write("첫번째\n두번째\n세번째\n")
+f.close()
+
+#파일읽기
+f = open("c:\\work\\test.txt", "rt", encoding="utf-8")
+print( f.read() )
+print( f.tell() )
+#다시 처음으로 돌아가기
+f.seek(0)
+lst = f.readlines()
+print(lst)
+#다시 처음으로 돌아가기
+f.seek(0)
+print("---한줄씩---")
+print( f.readline(), end="" )
+print( f.readline(), end="" )
+f.close() 
+
