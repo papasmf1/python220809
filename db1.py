915c494285df6d7dfd09a45c8d939961283dd075
papasmf1 <papasmf1@gmail.com>
2022-08-11 14:51:15 +0900

    데이터베이스 완료

--- db1.py
+++ db1.py
@@ -1,11 +1,11 @@
 # db1.py
 import sqlite3
-#연결객체 생성
-con = sqlite3.connect(":memory:")
+#연결객체 생성(물리적인 파일에 저장)
+con = sqlite3.connect("c:\\work\\sample.db")
 #커서객체 생성
 cur = con.cursor()
 #테이블 구조(테이블 스키마 생성)
-cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+cur.execute("create table PhoneBook (Name str, PhoneNum str);")
 
 #입력 파라메터 처리(웹페이지, 모바일앱)
 name = "gildong"
@@ -18,4 +18,7 @@ cur.execute("insert into PhoneBook values ('derick', '010-111');")
 cur.execute("select * from PhoneBook;")
 for row in cur:
     print(row)
+
+#작업을 정상적으로 완료(입력, 수정, 삭제)
+con.commit() 
 
