54612d48db9425838dea4aa96beed9ea83316fc9
papasmf1 <papasmf1@gmail.com>
2022-08-11 14:23:34 +0900

    데이터베이스 연습3

--- db1.py
+++ db1.py
@@ -6,6 +6,12 @@ con = sqlite3.connect(":memory:")
 cur = con.cursor()
 #테이블 구조(테이블 스키마 생성)
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+
+#입력 파라메터 처리(웹페이지, 모바일앱)
+name = "gildong"
+phoneNumber = "010-222"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber) )
+
 #1건 레코드(행)을 입력
 cur.execute("insert into PhoneBook values ('derick', '010-111');")
 #검색
