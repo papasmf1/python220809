61559590dc6630b586e5a33ce8804d4856242b19
papasmf1 <papasmf1@gmail.com>
2022-08-11 16:52:41 +0900

    웹 크롤링 연습2

--- web1.py
+++ web1.py
@@ -2,8 +2,30 @@
 #웹크롤링(데이터 수집)
 from bs4 import BeautifulSoup
 
+#메서드 체인:연속으로 함수나 메서드 호출
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체
 soup = BeautifulSoup(page, "html.parser")
-print( soup.prettify() )
+#print( soup.prettify() )
+#<p>태그를 몽땅 검색
+#print( soup.find_all("p") )
+#첫번째<p>태그 
+#print( soup.find("p") )
+#조건이 있는: <p class='outer-text'> 필터링 
+#파이썬의 키워드 class --> class_
+#print( soup.find_all("p", class_="outer-text") )
+
+#내부 문자열 추출
+for item in soup.find_all("p"):
+    #컨텐츠: ~.text
+    title = item.text.strip()
+    title = title.replace("\n", "")
+    print(title)
+
+    
+
+
+
+
+
 
